f0c2e40f6f0398a7766ecc32da14be90239fb583
amira <amirasalhi1@gmail.com>
2025-08-09 00:14:24 +0100

    modification ajout de projet quiz

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,7 @@ const Header = () => {
             <Link 
               to="about" 
               smooth={true} 
-              duration={1000} 
+              duration={1200} 
               spy={true} 
               offset={-60}
             >
@@ -23,7 +23,7 @@ const Header = () => {
             <Link 
               to="skills" 
               smooth={true} 
-              duration={1000} 
+              duration={1200} 
               spy={true} 
               offset={-60}
             >
@@ -34,7 +34,7 @@ const Header = () => {
             <Link 
               to="projects" 
               smooth={true} 
-              duration={1000} 
+              duration={1200} 
               spy={true} 
               offset={-60}
             >
@@ -45,7 +45,7 @@ const Header = () => {
             <Link 
               to="contact" 
               smooth={true} 
-              duration={1000} 
+              duration={1200} 
               spy={true} 
               offset={-60}
             >
--- src/components/projects.js
+++ src/components/projects.js
@@ -6,21 +6,29 @@ function Projects() {
 		  <section id="projects" tabIndex="-1" className="fade-in" aria-label="Projets réalisés">
 		  <h2>Projets</h2>
 		  <div className="projects-grid" role="list">
+			<article className="project-card" role="listitem" tabIndex="0" aria-label="Projet Quiz">
+			  <h3>Quiz Concours Informatique</h3>
+			  <p>En React js : quiz interactif avec 100 questions techniques aléatoires pour préparer le concours des techniciens en informatique.
+Interface fluide avec correction instantanée, démontrant mes compétences en développement front-end et déploiement. </p>
+			  <a className="btn-secondary" href="https://amirasalhi.github.io/react-portfolio/">Voir plus</a>
+			</article>
 			<article className="project-card" role="listitem" tabIndex="0" aria-label="Projet Portfolio">
+			  <h3>Mon Portfolio</h3>
+			  <p>En React js : Un site portfolio responsive réalisé avec React, présentant mes projets, compétences et expériences. L’interface met en avant une navigation fluide, des animations, et une section dynamique pour les projets. </p>
+			  <a className="btn-secondary" href="https://amirasalhi.github.io/react-portfolio/">Voir plus</a>
+			</article>
+			<article className="project-card" role="listitem" tabIndex="0" aria-label="Projet Meteo">
+			  <h3>Site de Meteo</h3>
+			  <p>En React js : Une application météo simple et responsive permettant d'afficher les conditions climatiques en temps réel pour n’importe quelle ville dans le monde.</p>
+			  <a className="btn-secondary" href="/react-portfolio/weather/">Voir plus</a>
+			</article>
+			<article className="project-card" role="listitem" tabIndex="0" aria-label="Projet Confessions">
 			  <h3>Site de confessions</h3>
 			  <p>Création d’un site responsive pour un particulier, avec animations CSS et optimisation SEO.</p>
 			  <a className="btn-secondary" href=" http://www.radar60.com">Voir plus </a>
 			</article>
-			<article className="project-card" role="listitem" tabIndex="0" aria-label="Projet Blog">
-			  <h3>Site de Meteo</h3>
-			  <p>Une application météo simple et responsive permettant d'afficher les conditions climatiques en temps réel pour n’importe quelle ville dans le monde.</p>
-			  <a className="btn-secondary" href="/react-portfolio/weather/">Voir plus</a>
-			</article>
-			<article className="project-card" role="listitem" tabIndex="0" aria-label="Projet Blog">
-			  <h3>Mon Portfolio</h3>
-			  <p> Un site portfolio responsive réalisé avec React, présentant mes projets, compétences et expériences. L’interface met en avant une navigation fluide, des animations, et une section dynamique pour les projets. </p>
-			  <a className="btn-secondary" href="https://amirasalhi.github.io/react-portfolio/">Voir plus</a>
-			</article>
+		
+
 		  </div>
 		</section>
     );
