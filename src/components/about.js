0c46aae47ebef5a03d2404fa50a4bbdc72813ef2
amira <amirasalhi1@gmail.com>
2026-04-10 23:12:10 +0100

    modif

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ const About = () => {
     <section id="about" className="fade-in" aria-label="À propos de Amira">
       <h2>À propos</h2>
       <p>
-       Passionnée par le développement front-end, je conçois des interfaces web modernes, fluides et soignées. J’attache une grande importance à la clarté du design, à l’expérience utilisateur, ainsi qu’à la qualité du code.
+       hello -Passionnée par le développement front-end, je conçois des interfaces web modernes, fluides et soignées. J’attache une grande importance à la clarté du design, à l’expérience utilisateur, ainsi qu’à la qualité du code.
 Ma démarche : allier esthétique, performance et accessibilité, en respectant les bonnes pratiques du développement moderne (HTML5, CSS3, JavaScript, animations, UI/UX).
 Ce portfolio reflète mon style, mes compétences et ma volonté constante de progresser à travers des projets concrets et engageants.
       </p>
