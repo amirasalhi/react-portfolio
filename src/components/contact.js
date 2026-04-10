dc95de612338b5e1648358ea80b84483f6f102e7
amira <amirasalhi1@gmail.com>
2026-04-10 22:17:48 +0100

    test

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,9 +9,9 @@ const Contact  = ({ showToast }) => {
 		<div className="contact_grid">
         <div className="contact-info">
         
-          <p><img className="icon_contact" src="images/mail.png"/> <span>amirasalhi1@gmail.com</span></p>
-          <p><img className="icon_contact" src="images/phone.png"/> <span>+212 6 07 89 61 72</span></p>
-          <p><img className="icon_contact" src="images/localisation.png"/><span> Maroc</span></p>
+          <p><img className="icon_contact" src="../images/mail.png" alt="" /> <span>amirasalhi1@gmail.com</span></p>
+          <p><img className="icon_contact" src="images/phone.png" alt="" /> <span>+212 6 07 89 61 72</span></p>
+          <p><img className="icon_contact" src="images/localisation.png" alt="" /><span> Maroc</span></p>
         </div>
 	     <ContactForm showToast={showToast}/>
 		</div>
--- src/components/skills.js
+++ src/components/skills.js
@@ -6,44 +6,44 @@ function Skills() {
 		  <h2>Compétences</h2>
 		  <div className="skills-grid" role="list">
 			<article className="skill" role="listitem" tabIndex="0" aria-label="seo">
-			  <span className="skill-icon" aria-hidden="true"><img src="images/react.png"/></span>
+			  <span className="skill-icon" aria-hidden="true"><img src="images/react.png" alt=""/></span>
 			  <h3>React</h3>
 			</article>
 			<article className="skill" role="listitem" tabIndex="0" aria-label="JavaScript">
-			  <span className="skill-icon" aria-hidden="true"><img src="images/js.png"/></span>
+			  <span className="skill-icon" aria-hidden="true"><img src="images/js.png" alt=""/></span>
 			  <h3>JavaScript</h3>
 
 			</article>
 			<article className="skill" role="listitem" tabIndex="0" aria-label="HTML5">
-			  <span className="skill-icon" aria-hidden="true"><img src="images/html5.png"/></span>
+			  <span className="skill-icon" aria-hidden="true"><img src="images/html5.png" alt=""/></span>
 			  <h3>HTML5</h3>
 
 			</article>
 			<article className="skill" role="listitem" tabIndex="0" aria-label="CSS3">
-			  <span className="skill-icon" aria-hidden="true"><img src="images/css3.png"/></span>
+			  <span className="skill-icon" aria-hidden="true"><img src="images/css3.png" alt=""/></span>
 			  <h3>CSS3</h3>
 
 			</article>
 
 		
 			<article className="skill" role="listitem" tabIndex="0" aria-label="Git">
-			  <span className="skill-icon" aria-hidden="true"><img src="images/github.png"/></span>
+			  <span className="skill-icon" aria-hidden="true"><img src="images/github.png" alt=""/></span>
 			  <h3>Git</h3>
 			</article>
 
 			<article className="skill" role="listitem" tabIndex="0" aria-label="figma">
-			  <span className="skill-icon" aria-hidden="true"><img src="images/figma.png"/></span>
+			  <span className="skill-icon" aria-hidden="true"><img src="images/figma.png" alt=""/></span>
 			  <h3>Figma</h3>
 
 			</article>
 				<article className="skill" role="listitem" tabIndex="0" aria-label="sass">
-			  <span className="skill-icon" aria-hidden="true"><img src="images/sass.png"/></span>
+			  <span className="skill-icon" aria-hidden="true"><img src="images/sass.png" alt=""/></span>
 			  <h3>SaSS</h3>
 
 			</article>
 					
 			<article className="skill" role="listitem" tabIndex="0" aria-label="ps">
-			  <span className="skill-icon" aria-hidden="true"><img src="images/ps.png"/></span>
+			  <span className="skill-icon" aria-hidden="true"><img src="public/images/ps.png" alt=""/></span>
 			  <h3>PhotoShop</h3>
 			</article>
 		
